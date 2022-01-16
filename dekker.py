0444f7f99dbb53b3fd62a7094fe8e98d4162f71e
mohammadrezarouhani <mohammadrezarouhani@protonmail.com>
2022-01-15 21:56:41 -0500

    dekker added/bin/python3

---
+++ dekker.py
@@ -0,0 +1,37 @@
+from threading import Thread
+from time import sleep
+from unicodedata import name
+
+turn=0
+
+def critical_section(process_num):
+    print("process {} entered critical section".format(process_num))
+
+def reminder_section(process_num):
+    print("process {} entered reminder section\n".format(process_num))
+
+def peterson(process_num):
+    j = (process_num+1)%2
+    global turn 
+
+    while True:
+        while not turn==process_num:
+            pass
+
+        critical_section(process_num)
+        turn=j
+        reminder_section(process_num)
+        sleep(2)
+
+
+if __name__=='__main__':
+    li=[]
+    for i in range(0,2):
+        li.append(Thread(target=peterson,args=(i,)))
+
+    for i in li:
+        i.start()
+    
+    for i in li:
+        i.join()
+    
